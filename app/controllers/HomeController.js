4ccb4ff4c08daf9d5e65c946a0a3442f102f832c
Rodrigo Mady <rodrigo.m.dasilva@bairesdev.com>
2022-01-11 17:56:31 -0300

    [ADD] Add new info and translate part for pt

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -22,9 +22,25 @@ var globalizeExpress = require('globalize-express'),
     renderHome = async (req, res) => {
         try {
             res.render("home", { 
-                title: req.Globalize.formatMessage('home/title'),
-                bio:   req.Globalize.formatMessage('home/bio'),   
-                welcome:   req.Globalize.formatMessage('home/welcome'),   
+                title:   req.Globalize.formatMessage('home/title'),
+                bio:     req.Globalize.formatMessage('home/bio'),   
+                welcome: req.Globalize.formatMessage('home/welcome'),
+                sitelanguage: req.Globalize.formatMessage('home/sitelanguage'),
+                aboutme:   req.Globalize.formatMessage('home/aboutme'),
+                portfolio: req.Globalize.formatMessage('home/portfolio'),
+                services:  req.Globalize.formatMessage('home/services'),
+                resume:    req.Globalize.formatMessage('home/resume'),
+                contact:    req.Globalize.formatMessage('home/contact'),
+                themetrema: req.Globalize.formatMessage('home/themetrema'),
+                purplemode: req.Globalize.formatMessage('home/purplemode'),
+                mainrole:   req.Globalize.formatMessage('home/mainrole'),
+                others:     req.Globalize.formatMessage('home/others'),
+                roledescription: req.Globalize.formatMessage('home/roledescription'),
+                skills: req.Globalize.formatMessage('home/skills'),
+                skillsdescription: req.Globalize.formatMessage('home/skillsdescription'),
+                jobs: req.Globalize.formatMessage('home/jobs'),
+                whatidid: req.Globalize.formatMessage('home/whatidid'),
+                ownbussiness: req.Globalize.formatMessage('home/ownbussiness'),
             });
         } catch (e) {
             console.log(e);
